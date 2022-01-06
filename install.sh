bf4a5ae51cc2d1aaad70de3c62126aeb4cb6f3bd
Ronaldo Gligan <ronaldodgg@outlook.com>
2022-01-06 23:02:23 +0100

    Added shebang

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
 
 ./build.sh &&
-sudo cp ./irida.exe /bin/irida
+sudo mkdir -p /usr/local/irida &&
+sudo cp -r ./libraries/ /usr/local/irida &&
+sudo cp -r ./irida.exe /usr/local/irida/irida &&
+sudo ln -s /usr/local/irida/irida /bin/irida
