a90e7c859f061c7e2a2bf4575d87b56302eeba31
Ronaldo Gligan <ronaldodgg@outlook.com>
2022-01-06 22:21:20 +0100

    Adapting templates to support C's conventions

--- lib/templates.ml
+++ lib/templates.ml
@@ -1,55 +1,16 @@
 
 let (@@) s1 s2 = s1 ^ "\n" ^ s2
 
-
 let header =
   "BITS 64"                              @@
-  "segment .text"                        @@
-  "label_PRINTVAL_init:"                 @@
-  "PRINTVAL:"                            @@
-  "    mov     r9, -3689348814741910323" @@
-  "    sub     rsp, 40"                  @@
-  "    mov     BYTE [rsp+31], 10"        @@
-  "    lea     rcx, [rsp+30]"            @@
-  ".L2:"                                 @@
-  "    mov     rax, rdi"                 @@
-  "    lea     r8, [rsp+32]"             @@
-  "    mul     r9"                       @@
-  "    mov     rax, rdi"                 @@
-  "    sub     r8, rcx"                  @@
-  "    shr     rdx, 3"                   @@
-  "    lea     rsi, [rdx+rdx*4]"         @@
-  "    add     rsi, rsi"                 @@
-  "    sub     rax, rsi"                 @@
-  "    add     eax, 48"                  @@
-  "    mov     BYTE [rcx], al"           @@
-  "    mov     rax, rdi"                 @@
-  "    mov     rdi, rdx"                 @@
-  "    mov     rdx, rcx"                 @@
-  "    sub     rcx, 1"                   @@
-  "    cmp     rax, 9"                   @@
-  "    ja      .L2"                      @@
-  "    lea     rax, [rsp+32]"            @@
-  "    mov     edi, 1"                   @@
-  "    sub     rdx, rax"                 @@
-  "    xor     eax, eax"                 @@
-  "    lea     rsi, [rsp+32+rdx]"        @@
-  "    mov     rdx, r8"                  @@
-  "    mov     rax, 1"                   @@
-  "    syscall"                          @@
-  "    add     rsp, 40"                  @@
-  "    ret"                              @@
-  "global _start"                        @@
-  "_start:"                              @@
+  "global    main"                       @@
+  "section   .text"                      @@
+  "main:"                                @@
   "    mov [args_ptr], rsp"              @@
   "    mov rax, ret_stack_end"           @@
   "    mov [ret_stack_rsp], rax"
 
 
-let exit_syscall =
-  "    mov rax, 60"       @@
-  "    mov rdi, 0"        @@
-  "    syscall"
 
 let bss_segment _mem_capacity =
   "segment .bss"          @@
@@ -70,14 +31,14 @@ let str_to_dec_val_list str =
 
 let footer (strings: (int * string) list) _mem_capacity =
   let str_to_asm (index, str) =
-    "    str_" ^ string_of_int index ^
+    "str_" ^ string_of_int index ^
     ": db " ^ str_to_dec_val_list str ^ "\n" in
 
-  exit_syscall            @@
-  "segment .data"         @@
+  "    ret" @@
   List.fold_left (fun res (i, str) ->
-    res ^ str_to_asm (i, str) ) "" strings @@
-  bss_segment _mem_capacity
+    res ^ str_to_asm (i, str) ) "" strings
+  @@ bss_segment _mem_capacity
+
 
 let pushInt i =
   let i' = Int.to_string i in
