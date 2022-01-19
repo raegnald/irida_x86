b5149a616571ccce2de3634bd1868dd73c94cf38
Ronaldo Gligan <ronaldodgg@outlook.com>
2022-01-19 18:02:33 +0100

    Added another symlink to executable

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@
 sudo mkdir -p /usr/local/irida &&
 sudo cp -r ./libraries/ /usr/local/irida &&
 sudo cp -r ./irida.exe /usr/local/irida/irida &&
+sudo ln -s /usr/local/irida/irida /usr/bin/irida &&
 sudo ln -s /usr/local/irida/irida /bin/irida
